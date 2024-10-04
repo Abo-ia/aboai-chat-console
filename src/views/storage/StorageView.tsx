554d7d19410d7a5c8be98daa57381a5878c572ab
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-10-03 19:32:36 -0600

    feat: update table

--- src/views/storage/StorageView.tsx
+++ src/views/storage/StorageView.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsV2Command, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import Sidebar from '@src/components/Storage/Sidebar';
 import Header from '@src/components/Storage/Header';
 import { FiFolder, FiFile, FiMoreVertical } from 'react-icons/fi';
 import { FaFolder, FaFile } from "react-icons/fa";
 
-
+// Función para organizar archivos por carpetas
 const organizeFilesByFolders = (items: any[]) => {
-    const folderStructure: { folder_path: string; files: { file_name: string; file_size: number }[] }[] = [];
+    const folderStructure: { folder_path: string; files: { file_name: string; file_size: number, key: string }[] }[] = [];
 
     items.forEach((item) => {
         const isFolder = item.Key.endsWith('/');
@@ -36,6 +36,7 @@ const organizeFilesByFolders = (items: any[]) => {
             folder.files.push({
                 file_name: fileName,
                 file_size: item.Size,
+                key: item.Key,
             });
         }
     });
@@ -47,7 +48,9 @@ const FileExplorerTable: React.FC = () => {
     const [organizedData, setOrganizedData] = useState<any[]>([]);
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [openFolders, setOpenFolders] = useState<string[]>([]); // Estado para manejar las carpetas abiertas
+    const [isDeleting, setIsDeleting] = useState<boolean>(false);  // Nuevo estado para eliminar archivos
+    const [openFolders, setOpenFolders] = useState<string[]>([]); 
+    const [openMenu, setOpenMenu] = useState<string | null>(null);  // Estado para controlar el menú abierto
     const itemsPerPage = 20;
 
     const s3Client = new S3Client({
@@ -83,6 +86,31 @@ const FileExplorerTable: React.FC = () => {
         );
     };
 
+    const toggleMenu = (fileKey: string) => {
+        setOpenMenu((prevMenu) => (prevMenu === fileKey ? null : fileKey)); // Abrir/cerrar el menú
+    };
+
+    const handleDeleteFile = async (fileKey: string) => {
+        setIsDeleting(true);  // Mostrar el mensaje de cargando cuando se inicia la eliminación
+        try {
+            const command = new DeleteObjectCommand({
+                Bucket: 'iabogado-bucket',
+                Key: fileKey,
+            });
+            await s3Client.send(command);
+            fetchFilesFromS3(); // Recargar los archivos después de la eliminación
+        } catch (error) {
+            console.error('Error deleting file:', error);
+        } finally {
+            setIsDeleting(false);  // Dejar de mostrar el mensaje de cargando después de la eliminación
+        }
+    };
+
+    const handleOpenFile = (fileKey: string) => {
+        const fileUrl = `https://iabogado-bucket.s3.amazonaws.com/${fileKey}`;
+        window.open(fileUrl, '_blank');
+    };
+
     useEffect(() => {
         fetchFilesFromS3();
     }, []);
@@ -94,14 +122,24 @@ const FileExplorerTable: React.FC = () => {
             <Sidebar />
             <div className="flex-1 flex flex-col">
                 <Header />
-                <div className="p-8 m-6 rounded-lg bg-white">
+                <div className="p-8 m-6 rounded-lg bg-white relative">
+                    {/* Alerta o mensaje flotante */}
+                    {isDeleting && (
+                        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 bg-red-500 text-white p-2 rounded-lg shadow-lg z-50">
+                            Eliminando archivo...
+                        </div>
+                    )}
+
                     <div className="overflow-y-auto max-h-[500px]"> {/* Scroll vertical y altura máxima */}
                         <table className="bg-white shadow-md rounded-lg min-w-full">
                             <thead>
                                 <tr className="border-b-[1px] border-gray-200">
                                     <th className="p-4 text-left text-custom-dark font-semibold">Nombre</th>
+                                    <th className="p-4 text-left text-custom-dark font-semibold">Propietario</th>
                                     <th className="p-4 text-left text-custom-dark font-semibold">Tamaño</th>
-                                    <th className="p-4 text-right text-custom-dark font-semibold">Acciones</th>
+                                    <th className="p-4 text-right text-custom-dark font-semibold">
+                                        <FiMoreVertical />
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -120,21 +158,50 @@ const FileExplorerTable: React.FC = () => {
                                         </tr>
 
                                         {openFolders.includes(folder.folder_path) &&
-                                            folder.files.map((file: any, fileIndex: any) => (
-                                                <tr
-                                                    key={`${folder.folder_path}-${fileIndex}`}
-                                                    className="border-b-[1px] border-gray-200"
-                                                >
-                                                    <td className="p-4 pl-12 flex items-center">
-                                                        <FaFile className="mr-2 text-custom-base" />
-                                                        {file.file_name}
-                                                    </td>
-                                                    <td className="p-4">{formatSizeInMB(file.file_size)} MB</td> {/* Mostrar tamaño en MB */}
-                                                    <td className="p-4 text-right">
-                                                        <FiMoreVertical className="cursor-pointer text-custom-base" />
-                                                    </td>
-                                                </tr>
-                                            ))}
+                                            folder.files.map((file: any, fileIndex: any) => {
+                                                return (
+                                                    <tr
+                                                        key={`${folder.folder_path}-${fileIndex}`}
+                                                        className="border-b-[1px] border-gray-200"
+                                                    >
+                                                        <td className="p-4 pl-12 flex items-center">
+                                                            <FaFile className="mr-2 text-custom-base" />
+                                                            {file.file_name}
+                                                        </td>
+                                                        <td className="p-4">
+                                                            <img
+                                                                className='w-5 h-5 rounded-full'
+                                                                src='https://cdn-icons-png.flaticon.com/512/1077/1077114.png'
+                                                            />
+                                                        </td>
+                                                        <td className="p-4">{formatSizeInMB(file.file_size)} MB</td>
+                                                        <td className="p-4 relative">
+                                                            <FiMoreVertical
+                                                                className="cursor-pointer text-custom-base"
+                                                                onClick={() => toggleMenu(file.key)}
+                                                            />
+                                                            {openMenu === file.key && (
+                                                                <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-10">
+                                                                    <ul>
+                                                                        <li
+                                                                            className="py-2 pl-4 cursor-pointer hover:bg-gray-100"
+                                                                            onClick={() => handleOpenFile(file.key)}
+                                                                        >
+                                                                            Abrir archivo
+                                                                        </li>
+                                                                        <li
+                                                                            className="py-2 pl-4 cursor-pointer hover:bg-gray-100"
+                                                                            onClick={() => handleDeleteFile(file.key)}
+                                                                        >
+                                                                            Eliminar archivo
+                                                                        </li>
+                                                                    </ul>
+                                                                </div>
+                                                            )}
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })}
                                     </React.Fragment>
                                 ))}
                             </tbody>
