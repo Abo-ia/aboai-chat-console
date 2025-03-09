8c18a81d65848cc94b65933b9a3da3d7622a6cf1
jrauljperez07 <jrauljperez02.dev@gmail.com>
2025-03-09 15:21:12 -0600

    feat: deploy app

--- src/views/payment/AvailablePlans.tsx
+++ src/views/payment/AvailablePlans.tsx
@@ -1,58 +1,20 @@
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-const AvailablePlans = () => {
-    const plans = [
-        {
-            name: 'Free',
-            price: '$0/month',
-            description: 'Para proyectos personales y pequeños equipos.',
-            features: [
-                '1 Usuario',
-                '1 Organización',
-                '1,000 consultas/mes',
-                '5GB de Almacenamiento',
-                'Soporte Comunitario',
-            ],
-        },
-        {
-            name: 'Básico',
-            price: '$29.99/mes',
-            description: 'Ideal para pequeños equipos y usuarios individuales.',
-            features: [
-                '5 Usuarios',
-                '1 Organización',
-                '10,000 consultas/mes',
-                '50GB de Almacenamiento',
-                'Soporte Comunitario',
-            ],
-        },
-        {
-            name: 'Pro',
-            price: '$99.99/mes',
-            description: 'Para equipos en crecimiento que necesitan más funciones.',
-            features: [
-                '20 Usuarios',
-                '5 Organizaciones',
-                '100,000 consultas/mes',
-                '500GB de Almacenamiento',
-                'Soporte Prioritario',
-            ],
-        },
-        {
-            name: 'Enterprise',
-            price: 'Precio Personalizado',
-            description: 'Soluciones adaptadas para grandes empresas.',
-            features: [
-                'Usuarios Ilimitados',
-                'Organizaciones Ilimitadas',
-                'Consultas Ilimitadas',
-                '2TB de Almacenamiento',
-                'Soporte Dedicado',
-            ],
-        },
-    ];
+interface AvailablePlans {
+    name: string;
+    price: string;
+    description: string;
+    features: string[];
+    redirectUrl: string;
+}
 
+interface AvailablePlansProps {
+    plans: AvailablePlans[];
+    onSelectPlan: (url: string) => void;
+}
+
+const AvailablePlans = ({ plans, onSelectPlan }: AvailablePlansProps) => {
     const carouselRef = useRef<HTMLDivElement>(null);
 
     const scrollLeft = () => {
@@ -69,71 +31,47 @@ const AvailablePlans = () => {
 
     return (
         <div className="max-w-5xl mx-auto p-6 flex flex-col bg-white rounded-lg">
-            <div className="">
-                <div className="max-w-6xl mx-auto text-center">
-                    <h2 className="text-4xl font-bold text-gray-900 mb-6">Planes Disponibles</h2>
-                    <p className="text-gray-600 text-lg mb-6">
-                        Elige el mejor plan que se adapte a tus necesidades.
-                    </p>
+            <div className="max-w-6xl mx-auto text-center">
+                
 
-                    {/* Botones de navegación*/}
-                    <div className="relative flex items-center justify-center">
-                        <button
-                            className="absolute left-0 z-10 p-2 bg-white shadow-md rounded-full text-gray-600 hover:bg-gray-200 transition"
-                            onClick={scrollLeft}
-                        >
-                            <FaChevronLeft size={24} />
-                        </button>
+                <div className="relative flex items-center justify-center">
 
-                        {/* Carrusel */}
-                        <div
-                            ref={carouselRef}
-                            className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-4 px-4 no-scrollbar"
-                        >
-                            {plans.map((plan) => (
-                                <div
-                                    key={plan.name}
-                                    className="flex-none w-72 bg-white shadow-lg rounded-lg p-6 border border-gray-200 snap-center"
+                    <div
+                        ref={carouselRef}
+                        className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-4 px-4 no-scrollbar"
+                    >
+                        {plans.map((plan) => (
+                            <div
+                                key={plan.name}
+                                className="flex-none w-72 bg-white shadow-lg rounded-lg p-6 border border-gray-200 snap-center"
+                            >
+                                <h3 className="text-2xl font-semibold text-gray-900 mb-4">
+                                    {plan.name}
+                                </h3>
+                                <p className="text-xl font-bold text-custom-primary mb-4">
+                                    {plan.price}
+                                </p>
+                                <p className="text-gray-600 mb-4">{plan.description}</p>
+                                <ul className="text-gray-700 space-y-2 mb-6">
+                                    {plan.features.map((feature, index) => (
+                                        <li key={index} className="flex items-center">
+                                            <span className="text-green-500 mr-2">✔</span>{' '}
+                                            {feature}
+                                        </li>
+                                    ))}
+                                </ul>
+                                <button
+                                    className="w-full bg-custom-primary text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
+                                    onClick={() => onSelectPlan(plan.redirectUrl)}
                                 >
-                                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">
-                                        {plan.name}
-                                    </h3>
-                                    <p className="text-xl font-bold text-custom-primary mb-4">
-                                        {plan.price}
-                                    </p>
-                                    <p className="text-gray-600 mb-4">{plan.description}</p>
-                                    <ul className="text-gray-700 space-y-2 mb-6">
-                                        {plan.features.map((feature, index) => (
-                                            <li key={index} className="flex items-center">
-                                                <span className="text-green-500 mr-2">✔</span>{' '}
-                                                {feature}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                    <button className="w-full bg-custom-primary text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition duration-300">
-                                        Elegir Plan
-                                    </button>
-                                </div>
-                            ))}
-                        </div>
-
-                        <button
-                            className="absolute right-0 z-10 p-2 bg-white shadow-md rounded-full text-gray-600 hover:bg-gray-200 transition"
-                            onClick={scrollRight}
-                        >
-                            <FaChevronRight size={24} />
-                        </button>
+                                    Elegir Plan
+                                </button>
+                            </div>
+                        ))}
                     </div>
+
                 </div>
             </div>
-
-            {/* Sección de Métodos de Pago y Resumen
-            <div className="grid grid-cols-[65%_35%] gap-4 mb-6">
-                <PaymentMethods />
-                <OrderSummary />
-                <NewPaymentForm />
-                <Promotions />
-            </div> */}
         </div>
     );
 };
--- src/views/payment/StepperPayment.tsx
+++ src/views/payment/StepperPayment.tsx
@@ -1,37 +1,69 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaCheck } from 'react-icons/fa';
-
+import Logo from '@src/assets/logo.svg';
 import AvailablePlans from './AvailablePlans';
-import PaymentForm from './PaymentForm';
 
-import Logo from '@src/assets/logo.svg'
+const plans = [
+    {
+        name: 'Free',
+        price: '$0/month',
+        description: 'Para proyectos personales y pequeños equipos.',
+        features: [
+            '1 Usuario',
+            '1 Organización',
+            '1,000 consultas/mes',
+            '5GB de Almacenamiento',
+            'Soporte Comunitario',
+        ],
+        redirectUrl: 'https://buy.stripe.com/test_8wM14g6jdaiJ5PObIJ'
+    },
+    {
+        name: 'Básico',
+        price: '$400/mes',
+        description: 'Ideal para pequeños equipos y usuarios individuales.',
+        features: [
+            '5 Usuarios',
+            '1 Organización',
+            '10,000 consultas/mes',
+            '50GB de Almacenamiento',
+            'Soporte Comunitario',
+        ],
+        redirectUrl: 'https://buy.stripe.com/test_7sIcMY0YTeyZa6428a'
+    },
+    {
+        name: 'Pro',
+        price: '$4000/mes',
+        description: 'Para equipos en crecimiento que necesitan más funciones.',
+        features: [
+            '20 Usuarios',
+            '5 Organizaciones',
+            '100,000 consultas/mes',
+            '500GB de Almacenamiento',
+            'Soporte Prioritario',
+        ],
+        redirectUrl: 'https://buy.stripe.com/test_8wMdR2dLF62tba83cf'
+    },
+    {
+        name: 'Enterprise',
+        price: 'Precio Personalizado',
+        description: 'Soluciones adaptadas para grandes empresas.',
+        features: [
+            'Usuarios Ilimitados',
+            'Organizaciones Ilimitadas',
+            'Consultas Ilimitadas',
+            '2TB de Almacenamiento',
+            'Soporte Dedicado',
+        ],
+        redirectUrl: '/contact/enterprise'
+    },
+];
 
 const StepperPayment = () => {
-    const [currentStep, setCurrentStep] = useState(0);
-
     const navigate = useNavigate();
-
-    const steps = [
-        { name: 'Planes disponibles', component: <AvailablePlans /> },
-        { name: 'Confirma tu pago', component: <PaymentForm /> },
-    ];
 
     useEffect(() => {
         document.title = 'Abo.AI - Suscripción';
-    }, [])
-
-    const goNext = () => {
-        if (currentStep < steps.length - 1) {
-            setCurrentStep((prev) => prev + 1);
-        }
-    };
-
-    const goBack = () => {
-        if (currentStep > 0) {
-            setCurrentStep((prev) => prev - 1);
-        }
-    };
+    }, []);
 
     return (
         <div className="max-w-6xl mx-auto bg-white p-6 rounded-lg h-screen flex flex-col">
@@ -39,58 +71,20 @@ const StepperPayment = () => {
                 <div className="flex justify-between items-center">
                     <div
                         onClick={() => navigate('/')}
-                        className="flex items-center gap-2 cursor-pointer">
-                        <img
-                            src={Logo}
-                            alt="Logo"
-                            className="h-10 w-auto"
-                        />
+                        className="flex items-center gap-2 cursor-pointer"
+                    >
+                        <img src={Logo} alt="Logo" className="h-10 w-auto" />
                         <span className="text-xl font-semibold text-gray-800">Abo.AI</span>
                     </div>
                 </div>
             </header>
+            <h2 className="text-4xl font-bold text-gray-900 mb-6 text-center">Planes Disponibles</h2>
+            <p className="text-gray-600 text-lg mb-6 text-center">
+                Elige el mejor plan que se adapte a tus necesidades.
+            </p>
 
-            <div className="sticky top-16 bg-white z-10 py-4 border-b">
-                <div className="max-w-lg mx-auto flex justify-between items-center mb-2">
-                    {steps.map((step, index) => (
-                        <div key={index} className="flex items-center">
-                            <div
-                                className={`w-8 h-8 flex items-center justify-center rounded-full font-semibold text-white transition-all ${currentStep >= index ? 'bg-custom-primary' : 'bg-gray-300'
-                                    }`}
-                            >
-                                {currentStep > index ? <FaCheck size={16} /> : index + 1}
-                            </div>
-                            {index < steps.length - 1 && (
-                                <div className="w-12 h-1 bg-gray-300 mx-2"></div>
-                            )}
-                        </div>
-                    ))}
-                </div>
-
-                <p className="text-center text-sm font-semibold text-gray-700">
-                    {steps[currentStep].name}
-                </p>
-            </div>
-
-            <div className="flex-1 overflow-y-auto px-2">
-                <div>{steps[currentStep].component}</div>
-
-                <div className="flex justify-between mt-6 max-w-6xl mx-auto ">
-                    <button
-                        onClick={goBack}
-                        disabled={currentStep === 0}
-                        className="px-4 py-2 rounded bg-gray-300 text-gray-700 disabled:opacity-50"
-                    >
-                        Atrás
-                    </button>
-                    <button
-                        onClick={goNext}
-                        disabled={currentStep === steps.length - 1}
-                        className="px-4 py-2 rounded bg-custom-primary text-white hover:bg-custom-secondary transition-all"
-                    >
-                        Siguiente
-                    </button>
-                </div>
+            <div className="flex- overflow-y-auto px-2">
+                <AvailablePlans plans={plans} onSelectPlan={(url) => window.location.href = url} />
             </div>
         </div>
     );
